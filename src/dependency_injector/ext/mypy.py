85ae5a1c2e19c83a7649fbd80a306042112d5af2
zerlok <denergytro@gmail.com>
2022-09-03 15:34:57 +0200

    WIP: positional argument check in provider constructor added

--- src/dependency_injector/ext/mypy.py
+++ src/dependency_injector/ext/mypy.py
@@ -1,7 +1,13 @@
 import typing as t
 
+from mypy.nodes import ArgKind, Argument, ClassDef, TypeInfo, Var
 from mypy.plugin import FunctionContext, Plugin
-from mypy.types import Type
+from mypy.subtypes import is_callable_compatible, is_subtype
+from mypy.types import CallableType, Instance, Type
+
+
+def strip_margin(s: str, margin: str = "|") -> str:
+    return "\n".join(l.strip(f" {margin}") for l in s.split("\n"))
 
 
 class DependencyInjectorPlugin(Plugin):
@@ -9,16 +15,97 @@ class DependencyInjectorPlugin(Plugin):
     def get_function_hook(self, fullname: str) -> t.Optional[t.Callable[[FunctionContext], Type]]:
         if "dependency_injector" in fullname:
             def handle(context: FunctionContext):
-                print(context)
-                # context.api.named_generic_type()
+                tf, tf_args, tf_kwargs = context.arg_types  # type: t.Sequence[CallableType], t.Sequence[Instance], t.Sequence[Instance]
+
+                base_func_type = tf[0]
+                pos_args = [
+                    (typ, kind, name)
+                    for typs, kinds in zip(
+                        context.arg_types[1:],
+                        context.arg_kinds[1:],
+                    )
+                    for typ, kind, name in zip(typs, kinds, base_func_type.arg_names)
+                    if kind == ArgKind.ARG_POS
+                ]
+
+                # TODO: build partial callables using positional and named arguments
+                partial_func_type_from_base1 = base_func_type.copy_modified(
+                    arg_types=base_func_type.arg_types[:len(pos_args)],
+                    arg_kinds=base_func_type.arg_kinds[:len(pos_args)],
+                    arg_names=base_func_type.arg_names[:len(pos_args)],
+                )
+                partial_func_type_from_base2 = base_func_type.copy_modified(
+                    arg_types=base_func_type.arg_types[len(pos_args):],
+                    arg_kinds=base_func_type.arg_kinds[len(pos_args):],
+                    arg_names=base_func_type.arg_names[len(pos_args):],
+                )
+
+                provider_callable_type = base_func_type.copy_modified(
+                    arg_types=[typ for typ, _, _ in pos_args],
+                    arg_kinds=[kind for _, kind, _ in pos_args],
+                    arg_names=[name for _, _, name in pos_args],
+                    ret_type=partial_func_type_from_base2,
+                )
+
+                if not is_callable_compatible(
+                        provider_callable_type,
+                        partial_func_type_from_base1,
+                        is_compat=is_subtype,
+                        check_args_covariantly=True,
+                        ignore_return=True,
+                ):
+                    # TODO: better error message
+                    context.api.msg.fail(
+                        msg=strip_margin(f"""
+                        | base_func_type: {base_func_type}
+                        | pos_args: {pos_args}
+                        | partial_func_type_from_base1: {partial_func_type_from_base1}
+                        | partial_func_type_from_base2: {partial_func_type_from_base2}
+                        | provider_callable_type: {provider_callable_type}
+                        | """),
+                        context=context.context,
+                    )
+
+                    # context.api.msg.could_not_infer_type_arguments(base_func_type, 1, context)
+                    return context.default_return_type
+
+                if isinstance(context.default_return_type, Instance):
+                    i: Instance = context.default_return_type
+                    ti: TypeInfo = i.type
+                    c: ClassDef = ti.defn
+
+                    provider_cls = ClassDef(
+                        name=ti.name + "Partial",
+                        defs=c.defs,
+                        type_vars=c.type_vars,
+                    )
+                    meth_args = [Argument(Var(name), typ, None, kind) for typ, kind, name in
+                                 zip(partial_func_type_from_base2.arg_types, partial_func_type_from_base2.arg_kinds,
+                                     partial_func_type_from_base2.arg_names, )]
+
+                    # FIXME: strange error
+                    #  add_method_to_class(
+                    #  File "mypy/plugins/common.py", line 119, in add_method_to_class
+                    #  AttributeError: attribute 'TypeInfo' of 'names' undefined
+                    # add_method_to_class(
+                    #     context.api,
+                    #     provider_cls,
+                    #     "__call__",
+                    #     meth_args,
+                    #     partial_func_type_from_base2.ret_type,
+                    # )
+                    #
+                    # return Instance(provider_cls, [])
+
                 return context.default_return_type
 
             return handle
 
-    # def get_function_hook(self, fullname: str) -> Optional[Callable[[FunctionContext], Type]]:
+    # def get_method_hook(self, fullname: str) -> Optional[Callable[[MethodContext], Type]]:
     #     if "dependency_injector" in fullname:
-    #         def handle(context: FunctionContext):
-    #             return context
+    #         def handle(context: MethodContext):
+    #             print(context, id(context.type))
+    #             return context.default_return_type
     #
     #         return handle
 
--- tests/mypy/test1.py
+++ tests/mypy/test1.py
@@ -1,30 +1,92 @@
-# mypy plugin should provide a type for dependency injector provider instances similar to a class below
-# import abc
-# import typing as t
-# class TripleMultiplierProvider(t.Protocol):
-#     @abc.abstractmethod
-#     def __call__(self, y: float) -> "TripleMultiplier":
-#         raise NotImplementedError
-
 from dependency_injector import providers
 
 
-class TripleMultiplier:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = x
-        self.y = y
-
-    def perform(self, z: float) -> float:
-        return self.x * self.y * z
+class T1:
+    pass
 
 
-invalid_x_provider: providers.Provider[TripleMultiplier] = providers.Factory(TripleMultiplier, "invalid")
-valid_x_provider: providers.Provider[TripleMultiplier] = providers.Factory(TripleMultiplier, 2.0)
+class T11(T1):
+    pass
 
-invalid_y_instance1: TripleMultiplier = valid_x_provider("invalid")
-invalid_y_instance2: TripleMultiplier = valid_x_provider(x=3.0)
-valid_y_instance1: TripleMultiplier = valid_x_provider(4.0)
-valid_y_instance2: TripleMultiplier = valid_x_provider(y=5.0)
 
-invalid_result1: float = valid_y_instance1.perform("invalid")
-valid_result1: float = valid_y_instance1.perform(6.0)
+class T111(T11):
+    pass
+
+
+class T2:
+    pass
+
+
+class T22(T2):
+    pass
+
+
+class T222(T22):
+    pass
+
+
+class T3:
+    pass
+
+
+class T33(T3):
+    pass
+
+
+class T333(T33):
+    pass
+
+
+class T4:
+    pass
+
+
+class T44(T4):
+    pass
+
+
+class T444(T44):
+    pass
+
+
+class Test1:
+    def __init__(self, x1: T1, x2: T2) -> None:
+        self.x1 = x1
+        self.x2 = x2
+
+    def action(self, x3: T3) -> T4:
+        assert isinstance(self.x1, T1) and isinstance(self.x2, T2) and isinstance(x3, T3)
+        return T4()
+
+
+class Test2:
+    def __init__(self, x1: T11, x2: T22) -> None:
+        self.x1 = x1
+        self.x2 = x2
+
+    def action(self, x3: T33) -> T44:
+        assert isinstance(self.x1, T11) and isinstance(self.x2, T22) and isinstance(x3, T33)
+        return T44()
+
+
+ok_p1: providers.Provider[Test1] = providers.Factory(Test1, T1())
+ok_p11: providers.Provider[Test1] = providers.Factory(Test1, T11())
+
+# TODO: make better error message
+err_p1: providers.Provider[Test1] = providers.Factory(Test1, T2())
+# TODO: check named args
+# err_p11: providers.Provider[Test1] = providers.Factory(Test1, x2=T11())
+
+# ok_p2: providers.Provider[Test1] = providers.Factory(Test1, x2=T2())
+# ok_p22: providers.Provider[Test1] = providers.Factory(Test1, x2=T22())
+
+ok_i1: Test1 = ok_p1(T2())
+# ok_i11: Test1 = ok_p1(T22())
+# ok_i111: Test1 = ok_p1(x2=T2())
+
+# TODO: check partial constructor args
+err_i1 = ok_p1(T1())
+
+# ok_r1: T4 = ok_i1.action(T3())
+# ok_r11: T4 = ok_i11.action(T3())
+# ok_r111: T4 = ok_i111.action(T33())
